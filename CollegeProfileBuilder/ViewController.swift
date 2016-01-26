5c197c2c31562dc05fe438aaedc2fcd5e651a1a0
rkalvani <student@rkalvani.local>
2016-01-26 12:27:22 -0600

    allow for entries to be added

--- CollegeProfileBuilder/ViewController.swift
+++ CollegeProfileBuilder/ViewController.swift
@@ -36,5 +36,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
     }
 
+    @IBAction func onTappedPlusButton(sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            textField.placeholder = "Add College Here"
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alert.addAction(cancelAction)
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (action) -> Void in
+            let cityTextField = alert.textFields![0] as UITextField
+            self.colleges.append(cityTextField.text!)
+            self.tableView.reloadData()
+        }
+            alert.addAction(addAction)
+            self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
 }
 
