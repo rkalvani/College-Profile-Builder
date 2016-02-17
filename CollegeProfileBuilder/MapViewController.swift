04d8a47bb47e714ebfc6ad8ef93182f94ee7dea9
rkalvani <student@rkalvani.local>
2016-02-17 12:26:50 -0600

    created new map view controller, and added appropriate outlets, and custom view controller class

---
+++ CollegeProfileBuilder/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  CollegeProfileBuilder
+//
+//  Created by rkalvani on 2/17/16.
+//  Copyright © 2016 rkalvani. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapTextField: UITextField!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+}
